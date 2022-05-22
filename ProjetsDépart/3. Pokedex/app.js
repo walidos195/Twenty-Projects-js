7178e9371552aca04e3271f8bb28237b0fead5e1
Walidos195 <walidos195@gmail.com>
2022-05-23 01:25:35 +0200

    third commit

--- ProjetsDépart/3. Pokedex/app.js
+++ ProjetsDépart/3. Pokedex/app.js
@@ -1,53 +1,67 @@
-const searchinput=document.querySelector('#search');
+const searchinput = document.querySelector('#search');
 
-
-function fetchPokemons(){
+var tab = {
+    name: "",
+    id: "",
+    url: ""
+};
+var pokemons=[];
+function fetchPokemons() {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-    .then(resp=>resp.json())
-    .then((jdata)=>{
-        console.log(jdata.results[0].url)
-        console.log(fetchImgPokemon(jdata.results[0].url));
-        
-        
-    })
-}
-function fetchImgPokemon(link){
-    
-    
-   return fetch(link)
-    .then(resp=>resp.json())
-    .then((jdata)=>{
-        
-    //fetch
-    
-    jdata=fetch(jdata.forms[0].url)
-    .then(resp=>resp.json())
-    .then((jdata)=>{
-        jdata= jdata.sprites.front_default; 
-    })
-    console.log(jdata)
-    //endfeetch
-    }
-    
-    )
+        .then(resp => resp.json())
+        .then((jdata) => {
+
+            jdata.results.forEach(element => {
+               
+                fetchPokemon(element.url, element.name);
+                pokemons.push(tab);
+            });
+        })
 }
 
-function search(pokemon){
+
+
+
+
+function fetchPokemon(link, name) {
+    fetch(link)
+        .then(resp => resp.json())
+        .then((jdata) => {
+            //fetch
+            fetch(jdata.forms[0].url)
+                .then(resp => resp.json())
+                .then((jdata) => {
+                    tab.id = jdata.id;
+                    tab.name = name;
+                    tab.url = jdata.sprites.front_default;
+                })
+            //endfeetch
+        }
+        )
+}
+
+
+
+
+
+
+
+function search(pokemon) {
 
     console.log(searchinput.value);
 
 }
 
 
-searchinput.addEventListener('input',(e)=>{
-    if(e.target.value===""){
-        
+searchinput.addEventListener('input', (e) => {
+    if (e.target.value === "") {
+
         e.target.parentNode.classList.remove("active-search");
     }
-    else{
+    else {
         e.target.parentNode.classList.add("active-search");
 
     }
     fetchPokemons();
     search()
-} );
+});
