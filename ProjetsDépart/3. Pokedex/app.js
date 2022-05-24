853c09908dbfd180578f8046962bc31a4149eda3
Walidos195 <walidos195@gmail.com>
2022-05-24 16:04:50 +0200

    third commit

--- ProjetsDépart/3. Pokedex/app.js
+++ ProjetsDépart/3. Pokedex/app.js
@@ -1,24 +1,148 @@
-const searchinput = document.querySelector('#search');
-
 var tab = {
     name: "",
     id: "",
     url: ""
 };
-var pokemons=[];
+var pokemons = [];
+const searchinput = document.querySelector('#search');
+const ulPokemons = document.querySelector('.pokemons');
+fetchPokemons();
 function fetchPokemons() {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
         .then(resp => resp.json())
         .then((jdata) => {
 
             jdata.results.forEach(element => {
-               
-                fetchPokemon(element.url, element.name);
-                pokemons.push(tab);
+
+
+                fetch(element.url)
+                    .then(resp => resp.json())
+                    .then((jdata) => {
+                        //fetch
+                        fetch(jdata.forms[0].url)
+                            .then(resp => resp.json())
+                            .then((jdata) => {
+                                tab.id = jdata.id;
+                                tab.name = element.name;
+                                tab.url = jdata.sprites.front_default;
+                                ulPokemons.appendChild(makePokemon(tab));
+                                pokemons.push(tab);
+                            })
+
+                    }
+                    )
+
+
+
+
+
+
+
+               // fetchPokemon(element.url, element.name);
             });
         })
 }
 
+
+
+function fetchPokemon(link, name) {
+    fetch(link)
+        .then(resp => resp.json())
+        .then((jdata) => {
+            //fetch
+            fetch(jdata.forms[0].url)
+                .then(resp => resp.json())
+                .then((jdata) => {
+                    tab.id = jdata.id;
+                    tab.name = name;
+                    tab.url = jdata.sprites.front_default;
+                })
+
+        }
+        )
+}
+
+
+function makePokemon(element) {
+    var li = document.createElement('li');
+    li.classList.add('pokemon');
+
+    var img = document.createElement('img');
+    img.src = element.url;
+
+    var name = document.createElement('h5');
+    name.innerHTML = element.name;
+
+    var p = document.createElement(p);
+    p.innerHTML = "ID# " + element.id;
+
+    li.appendChild(img);
+    li.appendChild(name);
+    li.appendChild(p);
+
+
+    //`<li class="pokemon"><img src="${element.url}"><h5>${element.name}</h5><p>ID# ${element.id}</p></li>`;
+    return li;
+}
+
+
+
+function fetchPokemon(link, name) {
+    fetch(link)
+        .then(resp => resp.json())
+        .then((jdata) => {
+            //fetch
+            fetch(jdata.forms[0].url)
+                .then(resp => resp.json())
+                .then((jdata) => {
+                    tab.id = jdata.id;
+                    tab.name = name;
+                    tab.url = jdata.sprites.front_default;
+                })
+            //endfeetch
+        }
+        )
+}
+
+
+
+
+function fetchPokemon(link, name) {
+    fetch(link)
+        .then(resp => resp.json())
+        .then((jdata) => {
+            //fetch
+            fetch(jdata.forms[0].url)
+                .then(resp => resp.json())
+                .then((jdata) => {
+                    tab.id = jdata.id;
+                    tab.name = name;
+                    tab.url = jdata.sprites.front_default;
+                })
+            //endfeetch
+        }
+        )
+}
+
+
+
+
+function fetchPokemon(link, name) {
+    fetch(link)
+        .then(resp => resp.json())
+        .then((jdata) => {
+            //fetch
+            fetch(jdata.forms[0].url)
+                .then(resp => resp.json())
+                .then((jdata) => {
+                    tab.id = jdata.id;
+                    tab.name = name;
+                    tab.url = jdata.sprites.front_default;
+                })
+            //endfeetch
+        }
+        )
+}
 
 
 
@@ -45,7 +169,6 @@ function fetchPokemon(link, name) {
 
 
 
-
 function search(pokemon) {
 
     console.log(searchinput.value);
@@ -62,6 +185,5 @@ searchinput.addEventListener('input', (e) => {
         e.target.parentNode.classList.add("active-search");
 
     }
-    fetchPokemons();
     search()
 });
